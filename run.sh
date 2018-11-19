9ea0ef2edc2499c78f90c03b2514bcc0b36ddc5d
cjy <cjy632258@hotmail.com>
2018-11-19 16:59:25 +0800

    update

--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ export PASS=http://$HOST
 export ORIGIN=http://$HOST
 fi
 
-if [ "$REFERER" == "" ]; then
-export REFERER=$PASS
+if [ "$PROXY_REFERER" == "" ]; then
+export PROXY_REFERER=$PASS
 fi
 
-envsubst '$ORIGIN,$HOST,$PASS,$SERVER_NAME,$REFERER' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/proxy.conf
+envsubst '$ORIGIN,$PROXY_HOST,$PASS,$SERVER_NAME,$PROXY_REFERER' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/proxy.conf
 
 nginx -g "daemon off;"
